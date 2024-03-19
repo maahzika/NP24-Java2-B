4ed0f539e3c0042979760d8364545f0e351c527e
matheus guilherme <118367707+maahzika@users.noreply.github.com>
2024-03-19 19:09:37 -0300

    Add files via upload

---
+++ NP24_Java2_B/src/br/com/senaisp/bauru/matheus/ConversaoTipos02.java
@@ -0,0 +1,15 @@
+package br.com.senaisp.bauru.matheus.secao04;
+
+public class ConversaoTipos02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte num1 = 127;
+		num1++;
+		num1++;
+		System.out.println(num1);
+		System.out.println((int)3.5);
+
+	}
+
+}

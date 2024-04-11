499917f3f79bd44d8193927ce7cc744c355d712a
maahzika <maahguilherme2013@hotmail.com>
2024-04-10 21:58:07 -0300

    Conteudo da aula do dia 10-04-2024

---
+++ ProjetoJavaFX_Licao09/src/module-info.java
@@ -0,0 +1,10 @@
+module ProjetoJavaFX_Licao09 {
+	requires javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires javafx.media;
+	
+	opens br.com.senaisp.bauru.matheus.classes to javafx.graphics,
+	javafx.controls, javafx.fxml, javafx.media;
+	
+}
